07a229f0106fe111fed07a01523ec59dbe49c43f
LobovaElizaveta <116665647+LobovaElizaveta@users.noreply.github.com>
2022-12-06 15:55:51 +0300

    Add files via upload

---
+++ 1.cs
@@ -0,0 +1,10 @@
+﻿int [] massive = {1, -7, 567, 89, 223};
+int count = 0;
+foreach(int el in massive)
+{
+   if (el>0)
+   {
+      count++;
+   }
+}
+Console.WriteLine($"Чисел больше нуля: {count}");
---
+++ 2.cs
@@ -0,0 +1,33 @@
+﻿int [] massive = GetArray(0, 100);
+Console.WriteLine($"[{String.Join(", ", massive)}]");
+
+Console.WriteLine($"Упорядоченный массив: [{String.Join(", ", RigthMass(massive))}]");
+
+int [] GetArray(int minEl, int maxEl)
+{
+   int [] array = new int [6];
+   for(int i = 0; i<array.Length; i++)
+   {
+      array[i] = new Random().Next(minEl, maxEl+1);
+   }
+   return array;
+}
+
+
+int [] RigthMass(int [] array)
+{
+   int temp;
+   for(int i =0; i<array.Length-1; i++)
+   {
+      for (int j =i+1; j<array.Length;j++)
+      {
+         if(array[j]<array[i])
+         {
+            temp = array[i];
+            array[i] = array[j];
+            array[j]=temp;
+         }
+      }
+   }
+   return array;
+}
